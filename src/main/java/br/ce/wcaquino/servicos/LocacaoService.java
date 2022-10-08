c3c4b0162315fb970a3a3e75da26ba984819e154
DLLM <DLLM@gft.com>
2022-10-08 19:22:40 -0300

    Exercicios do teste unitario

---
+++ src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
@@ -0,0 +1,35 @@
+package br.ce.wcaquino.servicos;
+
+import static br.ce.wcaquino.utils.DataUtils.adicionarDias;
+
+import java.util.Date;
+import java.util.List;
+
+import br.ce.wcaquino.entidades.Filme;
+import br.ce.wcaquino.entidades.Locacao;
+import br.ce.wcaquino.entidades.Usuario;
+
+public class LocacaoService {
+	
+	public Locacao alugarFilme(Usuario usuario, List<Filme> filmes) {
+		Locacao locacao = new Locacao();
+		locacao.setFilme(filmes);
+		locacao.setUsuario(usuario);
+		locacao.setDataLocacao(new Date());
+		locacao.setValor(filmes.getPrecoLocacao());
+
+		//Entrega no dia seguinte
+		Date dataEntrega = new Date();
+		dataEntrega = adicionarDias(dataEntrega, 1);
+		locacao.setDataRetorno(dataEntrega);
+		
+		//Salvando a locacao...	
+		//TODO adicionar método para salvar
+		
+		return locacao;
+	}
+
+	public static void main(String[] args) {
+		
+	}
+}
